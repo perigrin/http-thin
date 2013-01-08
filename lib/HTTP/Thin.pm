23a8a1fc4a03f13cf0334ebd7aca72c6aca02619
Chris Prather <chris@prather.org>
2013-01-08 13:46:23 -0500

    make it releaseable

--- lib/HTTP/Thin.pm
+++ lib/HTTP/Thin.pm
@@ -1,12 +1,22 @@
 package HTTP::Thin;
 use 5.12.1;
 use warnings;
+
+#ABSTRACT: A Thin Wrapper around HTTP::Tiny to play nice with HTTP::Message
+
 use parent qw(HTTP::Tiny);
 use Safe::Isa;
 use Class::Method::Modifiers;
 use HTTP::Response;
 
-our $VERSION = $HTTP::Tiny::VERSION;
+
+=method request
+
+In addition to the parameters documented in L<HTTP::Tiny>, C<HTTP::Thin> takes L<HTTP::Request> objects as well. 
+
+The return value is an L<HTTP::Response> object.
+
+=cut
 
 around request => sub {
         my ($next, $self, @args) = @_;
@@ -30,3 +40,23 @@ around request => sub {
 };
 
 1;
+__END__
+
+=head1 SYNOPSIS
+
+    use 5.12.1;
+    use HTTP::Request::Common
+    use HTTP::Thin;
+
+    say HTTP::Thin->new(GET 'http://example.com')->as_string;
+
+=head1 DESCRIPTION
+
+WARNING: This module is untested beyond the very basics. The implementation is simple enough that it shouldn't do evil things but, yeah it's still not approved for use by small children.
+
+C<HTTP::Thin> is a thin wrapper around L<HTTP::Tiny> adding the ability to pass in L<HTTP::Request> objects and get back L<HTTP::Response> objects. The maintainers of L<HTTP::Tiny>, justifiably, don't want to have to maintain compatibility but many other projects already consume the L<HTTP::Message> objects. This is just glue code doing what it does best.
+
+
+=head1 WHY?
+
+A conversation on IRC lead to C<mst>, C<ether>, and I agreeing that this would be a useful module but probably not worth the effort. I wrote it anyway to get it out of my head. 
