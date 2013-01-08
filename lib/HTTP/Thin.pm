497fdf11c33d31d198cfb0573f24fbcb5e0f92c0
Chris Prather <chris@prather.org>
2013-01-08 12:57:09 -0500

    use Safe::Isa rather than the ugly

--- lib/HTTP/Thin.pm
+++ lib/HTTP/Thin.pm
@@ -2,7 +2,7 @@ package HTTP::Thin;
 use 5.12.1;
 use warnings;
 use parent qw(HTTP::Tiny);
-use Scalar::Util qw(blessed);
+use Safe::Isa;
 use Class::Method::Modifiers;
 use HTTP::Response;
 
@@ -10,7 +10,7 @@ our $VERSION = $HTTP::Tiny::VERSION;
 
 around request => sub {
         my ($next, $self, @args) = @_;
-        if (@args == 1 && blessed($args[0]) && $args[0]->isa('HTTP::Request')) {
+        if (@args == 1 && $args[0]->$_isa('HTTP::Request')) {
                 my $options = {};
                 $options->{headers} = \%{ $args[0]->headers } if $args[0]->headers->as_string;
                 $options->{content} = $args[0]->content if $args[0]->content;
